2387ef43c1ac07f52e341c7460427d4b70cec2ac
unknown <JimWe@N3849.tmt.telital.com>
2015-04-23 12:49:59 -0400

    Updated to support rolling files.

--- log/log.go
+++ log/log.go
@@ -7,16 +7,23 @@ import (
 	"os"
 	"runtime"
 	"strings"
+	"sync"
 	"time"
 )
 
 var fileHandle *os.File
 var writer io.Writer
 var maxLevel LogLevel
+var logFileName string
 
 var StripPackage bool
 var PadLocation bool
 var HideLocation bool
+
+var RollingFileSize int64 = 1048576
+var CurrentFileSize int64
+var RollingFileMux sync.Mutex
+var RollingFileCount int = 10
 
 const (
 	Always = LogLevel(iota)
@@ -29,32 +36,46 @@ const (
 
 type LogLevel uint8
 
-func Configure(fileName string, appendFile bool) error {
+func Configure(fileName string, rollingFileSize int64, rollingFileCount int) error {
 	StripPackage = true
 	PadLocation = false
 	HideLocation = false
 
+	RollingFileSize = rollingFileSize
+	RollingFileCount = rollingFileCount
+	
+	logFileName = fileName
+
 	if len(fileName) > 0 {
-		fh, e := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0660)
-		if e != nil {
-			if appendFile {
-				fh, e = os.OpenFile(fileName, os.O_RDWR|os.O_APPEND, 0660)
-			} else {
-				e = os.Rename(fileName, fileName+time.Now().Format(".20060102-150405"))
-				fh, e = os.OpenFile(fileName, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0660)
-			}
+
+		fi, err := os.Stat(fileName)
+		if err == nil {
+			CurrentFileSize = fi.Size()
 		}
+
+		openFile(fileName)
+
+	} else {
+		writer = os.Stdout
+		golog.SetFlags(golog.Ldate | golog.Ltime)
+		golog.SetOutput(writer)
+	}
+
+	maxLevel = Info
+	return nil
+}
+
+func openFile(fileName string) error {
+	fh, e := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0660)
+	if e != nil {
+		fh, e = os.OpenFile(fileName, os.O_RDWR|os.O_APPEND, 0660)
 		if e != nil {
 			return e
 		}
-		fileHandle = fh
-		writer = io.MultiWriter(fh, os.Stdout)
-	} else {
-		writer = os.Stdout
 	}
-	golog.SetFlags(golog.Ldate|golog.Ltime)
+	fileHandle = fh
+	writer = io.MultiWriter(fh, os.Stdout)
 	golog.SetOutput(writer)
-	maxLevel = Info
 	return nil
 }
 
@@ -96,11 +117,12 @@ func IsEnabled(level LogLevel) bool {
 }
 
 type LogMessage struct {
-	Level LogLevel   `json:"level"`
-	Ts time.Time     `json:"ts"`
-	Where string     `json:"where"`
-	Msg string       `json:"msg"`
+	Level LogLevel  `json:"level"`
+	Ts    time.Time `json:"ts"`
+	Where string    `json:"where"`
+	Msg   string    `json:"msg"`
 }
+
 var OnLogChan chan *LogMessage
 
 func Printf(level LogLevel, format string, v ...interface{}) {
@@ -132,7 +154,9 @@ func Printf(level LogLevel, format string, v ...interface{}) {
 		levelString = "[T] "
 		break
 	}
-	
+
+	var finalMsg string
+
 	//golog.Printf(levelString+f.Name()+"(): "+format,v...)
 	if !HideLocation {
 		var where string
@@ -141,22 +165,48 @@ func Printf(level LogLevel, format string, v ...interface{}) {
 		} else {
 			where = f.Name()
 		}
-		
-		if OnLogChan!=nil {
-			OnLogChan <- &LogMessage{ level,time.Now(),where,fmt.Sprintf(format,v...)}
+
+		if OnLogChan != nil {
+			OnLogChan <- &LogMessage{level, time.Now(), where, fmt.Sprintf(format, v...)}
 		}
 
 		if PadLocation && len(where) < 32 {
 			where = fmt.Sprintf("%-32s", where)
 		}
 
-		golog.Printf(levelString+"["+where+"] "+format, v...)
+		finalMsg = fmt.Sprintf(levelString+"["+where+"] "+format, v...)
 	} else {
-	
-		if OnLogChan!=nil {
-			OnLogChan <- &LogMessage{ level,time.Now(),"",fmt.Sprintf(format,v...)}
+
+		if OnLogChan != nil {
+			OnLogChan <- &LogMessage{level, time.Now(), "", fmt.Sprintf(format, v...)}
 		}
-		
-		golog.Printf(levelString+format, v...)
+
+		finalMsg = fmt.Sprintf(levelString+format, v...)
+	}
+	golog.Printf(finalMsg)
+
+	if RollingFileSize > 0 {
+		RollingFileMux.Lock()
+		CurrentFileSize += int64(len(finalMsg))
+
+		if CurrentFileSize > RollingFileSize {
+
+			fileHandle.Close()
+			rollLogs()
+
+			openFile(logFileName)
+			CurrentFileSize = 0
+		}
+		RollingFileMux.Unlock()
+	}
+}
+
+func rollLogs() {
+	for i := RollingFileCount; i > 0; i-- {
+		if i == 1 {
+			os.Rename(logFileName, logFileName+fmt.Sprintf(".%d", i))
+		} else {
+			os.Rename(logFileName+fmt.Sprintf(".%d", i-1), logFileName+fmt.Sprintf(".%d", i))
+		}
 	}
 }
